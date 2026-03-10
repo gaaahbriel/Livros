b7015c86cd23721035b99688130612254412d09d
Gaaahbriel <gabriel.bonfim111@gmail.com>
2026-03-10 17:56:02 -0300

    Finalizando avaliação dos livros

--- controllers/livro.controller.php
+++ controllers/livro.controller.php
@@ -6,4 +6,10 @@ $livro = $database->query(
     params: ["id" => $_GET['id']]
 )->fetch();
 
-view('livro', compact('livro'));
+$avaliacoes = $database->query(
+    query: "SELECT * FROM avaliacoes a JOIN usuarios u on u.id = a.usuario_id WHERE livro_id = :id",
+    class: Avaliacao::class,
+    params:['id' => $_GET['id']]
+)->fetchAll();
+
+view('livro', compact('livro','avaliacoes'));
--- views/livro.view.php
+++ views/livro.view.php
@@ -1,4 +1,20 @@
     <!-- Main -->
+    <?php
+    $sumNotas = array_reduce($avaliacoes, function ($carry, $a) {
+        return ($carry ?? 0) + $a->nota;
+    }) ?? 0;
+
+    $qtdAvaliacoes = count($avaliacoes);
+
+    if ($qtdAvaliacoes > 0) {
+        $media = round($sumNotas / $qtdAvaliacoes);
+    } else {
+        $media = 0;
+    }
+
+    $notaFinal = str_repeat('⭐', $media);
+    ?>
+
     <?= $livro->titulo ?>
     <div class="p-2 rounded border-stone-800 border-2 bg-stone-900">
         <div class="flex">
@@ -6,7 +22,7 @@
             <div class="space-y-1">
                 <a href="/livro?id=<?= $livro->id ?>" class="font-semibold hover:underline"><?= $livro->titulo ?></a>
                 <div class="text-cs italic"><?= $livro->autor ?></div>
-                <div class="text-cs italic">Avaliações</div>
+                <div class="text-cs italic"> <?= $notaFinal ?> (<?= count($avaliacoes) ?> Avaliações)</div>
             </div>
         </div>
 
@@ -18,42 +34,53 @@
     <h2>Avaliações</h2>
 
     <div class="grid grid-cols-4 gap-4">
-        <div class="col-span-3">Lista</div>
+        <div class="col-span-3 gap-4 grid">
+            <?php foreach ($avaliacoes as $avaliacao): ?>
+                <div class="border border-stone-700 rounded">
+                    <div>
+                    <?= $avaliacao->nome ?>
+                    </div>
+                    <?= $avaliacao->avaliacao ?>
+                    <?php $nota = str_repeat("⭐", $avaliacao->nota) ?>
+                    <?= $nota ?>
+                </div>
+            <?php endforeach; ?>
+        </div>
         <div>
-        <?php if(auth()): ?>
-            <div class="border border-stone-700 rounded">
-                <h1 class="border-b border-stone-700 text-stone-400 font-bold px-4 py-2">Me conte o que você achou:</h1>
-                <form class="p-4 space-y-4" method="POST" action="/avaliacao-criar">
-                    <?php if ($validacoes = flash()->get('validacoes')): ?>
-                        <div class="border-red-800 bg-red-900 text-red-400 px-4 py-1 rounded-md border-2">
-                            <ul>
-                                <li>dê uma olhada nos erros abaixo</li>
-                                <?php foreach ($validacoes as $validacao): ?>
-                                    <li><?= $validacao ?></li>
-                                <?php endforeach; ?>
-                            </ul>
+            <?php if (auth()): ?>
+                <div class="border border-stone-700 rounded">
+                    <h1 class="border-b border-stone-700 text-stone-400 font-bold px-4 py-2">Me conte o que você achou:</h1>
+                    <form class="p-4 space-y-4" method="POST" action="/avaliacao-criar">
+                        <?php if ($validacoes = flash()->get('validacoes')): ?>
+                            <div class="border-red-800 bg-red-900 text-red-400 px-4 py-1 rounded-md border-2">
+                                <ul>
+                                    <li>dê uma olhada nos erros abaixo</li>
+                                    <?php foreach ($validacoes as $validacao): ?>
+                                        <li><?= $validacao ?></li>
+                                    <?php endforeach; ?>
+                                </ul>
+                            </div>
+                        <?php endif; ?>
+                        <div class="flex flex-col">
+                            <input type="hidden" name="livro_id" value="<?= $livro->id ?>  ?>">
+                            <label class="text-stone-400 mb-1 text-xl" for="">Avaliação</label>
+                            <textarea type="text" name="avaliacao" class="border-stone-800 border-2 rounded-md bg-stone-900 focus:outline-none px-2 py-1" placeholder="Avaliação"></textarea>
                         </div>
-                    <?php endif; ?>
-                    <div class="flex flex-col">
-                        <input type="hidden" name="livro_id" value="<?= $livro->id ?>  ?>">
-                        <label class="text-stone-400 mb-1 text-xl" for="">Avaliação</label>
-                        <textarea type="text" name="avaliacao" class="border-stone-800 border-2 rounded-md bg-stone-900 focus:outline-none px-2 py-1" placeholder="Avaliação"></textarea>
-                    </div>
-                    <div class="flex flex-col">
-                        <label class="text-stone-400 mb-1 text-xl" for="">Nota</label>
-                        <select name="nota" class="border-stone-800 border-2 rounded-md bg-stone-900 focus:outline-none px-2 py-1">
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                            <option value="4">4</option>
-                            <option value="5">5</option>
-                        </select>
-                    </div>
-                    <button type="submit" class="border-stone-800 bg-stone-900 text-stone-400 px-4 py-1 rounded-md border-2 hover:bg-stone-700">
-                        Salvar
-                    </button>
-                </form>
-            </div>
-        <?php endif; ?>    
+                        <div class="flex flex-col">
+                            <label class="text-stone-400 mb-1 text-xl" for="">Nota</label>
+                            <select name="nota" class="border-stone-800 border-2 rounded-md bg-stone-900 focus:outline-none px-2 py-1">
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                            </select>
+                        </div>
+                        <button type="submit" class="border-stone-800 bg-stone-900 text-stone-400 px-4 py-1 rounded-md border-2 hover:bg-stone-700">
+                            Salvar
+                        </button>
+                    </form>
+                </div>
+            <?php endif; ?>
         </div>
     </div>
